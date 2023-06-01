8aa3309b32b1352897d1271061899e7d10fb805d
isimora <80853728+isimora@users.noreply.github.com>
2023-06-01 02:30:14 -0400

    arreglo de diccionarios y avance de producir orden

--- src/routes/ordenCompra.js
+++ src/routes/ordenCompra.js
@@ -10,32 +10,96 @@ const moment = require("moment");
 const Productdictionary = {};
 const Formuladictionary = {}; // Declara la variable dictionary fuera de la función
 
-async function getCSVDictionary(dictionary, filePath) {
+// Esta función lee el archivo CSV y llena el diccionario FORMULA
+async function getCSVDictionaryFormula(dictionary, filePath) {
   console.log("get CSV file...");
-
   try {
-    await readCSVFile(filePath, dictionary); // Espera a que se complete la lectura del archivo y se llene el diccionario
+    await readCSVFileFormula(filePath, dictionary); // Espera a que se complete la lectura del archivo y se llene el diccionario
     //console.log(dictionary); // Ahora puedes usar dictionary fuera de la función
     // Continúa con el resto del código que depende del diccionario
-
   } catch (error) {
     console.error(error);
   }
 }
 
-function readCSVFile(filePath, dictionary) {
+// Esta función lee el archivo CSV y llena el diccionario PRODUCTS
+async function getCSVDictionaryProducts(dictionary, filePath) {
+  console.log("get CSV file...");
+  try {
+    await readCSVFileProducts(filePath, dictionary); // Espera a que se complete la lectura del archivo y se llene el diccionario
+    //console.log(dictionary); // Ahora puedes usar dictionary fuera de la función
+    // Continúa con el resto del código que depende del diccionario
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+function readCSVFileFormula(filePath, dictionary) {
   return new Promise((resolve, reject) => {
     fs.createReadStream(filePath)
       .pipe(csv())
       .on("data", (row) => {
         const keys = Object.keys(row);
-        const key = row[keys[0]].toString();
-        const values = keys.slice(1).map((column) => row[column]);
-        if (!dictionary[key]) {
-          dictionary[key] = []; // Inicializa un array vacío si la clave no existe aún en el diccionario
-        }
-        dictionary[key].push(values); // Agrega los valores al array de la clave correspondiente        
+        const burgerId = row[keys[0]].toString();
+        const burgerName = row[keys[1]].toString();
+        const ingredientId = row[keys[2]].toString();
+        const ingredientName = row[keys[3]].toString();
         
+        if (!dictionary[burgerId]) {
+          dictionary[burgerId] = {
+            nombre: burgerName,
+            ingredientes: {} // Inicializa un nuevo objeto para los ingredientes de la hamburguesa
+          };
+        }
+        
+        if (!dictionary[burgerId].ingredientes[ingredientId]) {
+          dictionary[burgerId].ingredientes[ingredientId] = ingredientName; // Agrega el ingrediente al diccionario de ingredientes de la hamburguesa si no existe aún
+        }
+        
+      })
+      .on("end", () => {
+        resolve();
+      })
+      .on("error", (error) => {
+        reject(error);
+      });
+  });
+}
+
+function readCSVFileProducts(filePath, dictionary) {
+  return new Promise((resolve, reject) => {
+    fs.createReadStream(filePath)
+      .pipe(csv())
+      .on("data", (row) => {
+        const keys = Object.keys(row);
+        const sku = row[keys[0]].toString();
+        const name = row[keys[1]].toString();
+        const description = row[keys[2]].toString()
+        const productionCost = row[keys[3]].toString();
+        const numberIngredients = row[keys[4]].toString();
+        const expectedDuration = row[keys[5]].toString();
+        const productionBatch = row[keys[6]].toString();
+        const productionTime = row[keys[7]].toString();
+        const productionGroups = row[keys[8]].toString();
+        const totalGroups = row[keys[9]].toString();
+        const production = row[keys[10]].toString();
+        
+        if (!dictionary[sku]) {
+          dictionary[sku] = {
+            nombre: name,
+            descripcion: description,
+            costoProduccion: productionCost,
+            numeroIngredientes: numberIngredients,
+            duracionEsperada: expectedDuration,
+            loteProduccion: productionBatch,
+            tiempoProduccion: productionTime,
+            gruposProductores: productionGroups,
+            totalGruposProductores: totalGroups,
+            produccion: production
+          };
+        }
+        
+
         
       })
       .on("end", () => {
@@ -48,8 +112,8 @@ function readCSVFile(filePath, dictionary) {
 }
 
 // Llama a la función getCSVDictionary para comenzar la carga del diccionario
-getCSVDictionary(Productdictionary, "./products_E2.csv");
-//getCSVDictionary(Formuladictionary, "./formulas_E2.csv");
+getCSVDictionaryProducts(Productdictionary, "./products_E2.csv");
+getCSVDictionaryFormula(Formuladictionary, "./formulas_E2.csv");
 
 // Esta es una funcion para obtener el token, la usamos para hacer los llamados a la API (necesitan el token como autorizacion)
 async function getToken() {
@@ -186,13 +250,10 @@ const notificarActualizacion = async (data) => {
 };
 
 
-
-
 async function producir_orden(idOrden){
     //obtener orden
-
     try {
-        const token = await getToken();
+      const token = await getToken();
         const headers = {
             "Content-Type": "application/json", // Adjust the content type if necessary
             Authorization: "Bearer " + token,
@@ -200,12 +261,22 @@ async function producir_orden(idOrden){
           const response = await axios.get(
             `https://dev.api-proyecto.2023-1.tallerdeintegracion.cl/ordenes-compra/ordenes/${idOrden}`,
             {headers})
-        sku = response.data.sku
-        //console.log(sku)
+        
+            const sku = response.data.sku
+        console.log("HOLAAA",sku)
         const producto = Productdictionary[sku];
         // si es una hamburguesa debiera tener una formula que esta en Formulasdictionary
-        if(producto[9] === "cocina"){
+        console.log("RESPONDE", producto)
+        if(producto.produccion === "cocina"){
           //buscar en formulas
+          console.log("BURGAAA")
+          console.log(Formuladictionary[sku])
+          for (skuIngrediente in Formuladictionary[sku].ingredientes){
+            
+            ingrediente = Productdictionary[skuIngrediente];
+            const producirRouter = require('./producir');
+            const router = producirRouter({getToken, skuIngrediente, ingrediente});
+          }
         } 
 
     }
@@ -214,7 +285,7 @@ async function producir_orden(idOrden){
     }
 }
 
-//producir_orden("6470ffd12abc3cdd7509ff9d")
+producir_orden("6470ffd12abc3cdd7509ff9d")
 
 module.exports = router;
 
--- src/routes/producir.js
+++ src/routes/producir.js
@@ -1,17 +1,18 @@
 const Router = require('koa-router');
 const axios = require("axios");
 
-module.exports = function ({getToken}){
+module.exports = function ({getToken, sku, ingrediente}){
     const router = new Router();
     router.get('/producir', async (ctx) => {
         try {
+          console.log("INGREDIENTE", ingrediente)
             const token = await getToken();
             const headers = {
                 "Content-Type": "application/json", // Adjust the content type if necessary
                 Authorization: "Bearer " + token,
               };
-            const sku = "cd4b7aab6f"
-            const quantity = 4 
+            const sku = sku
+            const quantity = ingrediente.loteProduccion 
             const response = await axios.post(
                 "https://dev.api-proyecto.2023-1.tallerdeintegracion.cl//warehouse/products ",
                 { sku: `${sku}`, quantity: `${quantity}` },
